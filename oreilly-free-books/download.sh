c478f60d77b0b1e3ffd71fb775944ab9858fa41b
Luo Hao <luohao.brian@gmail.com>
2018-06-26 14:47:42 +0800

    Add oreilly free books downloading script

---
+++ oreilly-free-books/download.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# PDF downloads
+wget http://www.oreilly.com/programming/free/files/modern-java-ee-design-patterns.pdf
+wget http://www.oreilly.com/programming/free/files/object-oriented-vs-functional-programming.pdf
+wget http://www.oreilly.com/programming/free/files/java-the-legend.pdf
+wget http://www.oreilly.com/programming/free/files/introducing-java-8.pdf
+wget http://www.oreilly.com/programming/free/files/a-whirlwind-tour-of-python.pdf
+wget http://www.oreilly.com/programming/free/files/20-python-libraries-you-arent-using-but-should.pdf
+wget http://www.oreilly.com/programming/free/files/hadoop-with-python.pdf
+wget http://www.oreilly.com/programming/free/files/how-to-make-mistakes-in-python.pdf
+wget http://www.oreilly.com/programming/free/files/functional-programming-python.pdf
+wget http://www.oreilly.com/programming/free/files/python-in-education.pdf
+wget http://www.oreilly.com/programming/free/files/from-future-import-python.pdf
+wget http://www.oreilly.com/programming/free/files/trends-shaping-the-london-tech-scene.pdf
+wget http://www.oreilly.com/programming/free/files/2016-european-software-development-salary-survey.pdf
+wget http://www.oreilly.com/programming/free/files/2016-software-development-salary-survey-report.pdf
+wget http://www.oreilly.com/programming/free/files/why-rust.pdf
+wget http://www.oreilly.com/programming/free/files/c++-today.pdf
+wget http://www.oreilly.com/programming/free/files/analyzing-visualizing-data-f-sharp.pdf
+wget http://www.oreilly.com/programming/free/files/rxjava-for-android-app-development.pdf
+wget http://www.oreilly.com/programming/free/files/swift-pocket-reference.pdf
+wget http://www.oreilly.com/programming/free/files/ten-steps-to-linux-survival.pdf
+wget http://www.oreilly.com/programming/free/files/open-by-design.pdf
+wget http://www.oreilly.com/programming/free/files/getting-started-with-innersource.pdf
+wget http://www.oreilly.com/programming/free/files/microservices-antipatterns-and-pitfalls.pdf
+wget http://www.oreilly.com/programming/free/files/microservices-vs-service-oriented-architecture.pdf
+wget http://www.oreilly.com/programming/free/files/software-architecture-patterns.pdf
+wget http://www.oreilly.com/programming/free/files/migrating-cloud-native-application-architectures.pdf
+wget http://www.oreilly.com/programming/free/files/reactive-microservices-architecture-orm.pdf
+wget http://www.oreilly.com/programming/free/files/engineering-managers-guide-design-patterns.pdf
+wget http://www.oreilly.com/programming/free/files/azure-for-developers.pdf
+wget http://www.oreilly.com/iot/free/files/ambient-computing.pdf
+wget http://www.oreilly.com/iot/free/files/governing-the-iot.pdf
+wget http://www.oreilly.com/iot/free/files/iot-opportunities-challenges.pdf
+wget http://www.oreilly.com/iot/free/files/creating-functional-teams-for-iot.pdf
+wget http://www.oreilly.com/iot/free/files/building-a-hardware-business.pdf
+wget http://www.oreilly.com/iot/free/files/when-hardware-meets-software.pdf    
+wget http://www.oreilly.com/iot/free/files/what-is-the-internet-of-things.pdf
+wget http://www.oreilly.com/iot/free/files/software-above-device.pdf
+wget http://www.oreilly.com/iot/free/files/software-hardware-collide.pdf
+wget http://www.oreilly.com/iot/free/files/hardware-by-the-numbers.pdf
